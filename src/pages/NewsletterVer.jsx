ee9cfe847f18702303d66ae293e312982942702c
Julian Kleber <cap_jmk@MBP-von-Julian.fritz.box>
2022-05-16 11:49:17 +0200

    recognizes token in search string

--- src/pages/NewsletterVer.jsx
+++ src/pages/NewsletterVer.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-class NewsletterVer extends Component {
-    render() {
-        const { token } = this.props.match.params;
-        return (
-            <div>Token: { token }</div>
-        );
-    }
-};
+import React from "react"
+import { useSearchParams } from "react-router-dom"
+
+const NewsletterVer = () => {
+  let [searchParams, setSearchParams] = useSearchParams()
+  const token= searchParams.get("token")
+
+  return (
+    <div className="App">
+      <p>Value of token: {token}</p>
+    </div>
+  )
+}
+
 export default NewsletterVer;
